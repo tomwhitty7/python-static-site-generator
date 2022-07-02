3440316a1cb3f5d7b66b70d5fbf6651897b3c895
Tom Whitty <tomwhitty7@gmail.com>
2022-07-02 15:05:49 -0500

    m3t2

--- ssg/content.py
+++ ssg/content.py
@@ -1,30 +1,43 @@
 import re
-from yaml import load FullLoader
 
 from collections.abc import Mapping
+from yaml import load, FullLoader
 
 class Content(Mapping):
-    __delimeter = "^(?:-|\+){3}\s*$"
+    __delimeter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__deliiter, re.MULTILINE)
 
+    @classmethod
     def load(cls, string):
-        _, fm, content = split(__regex, string, 2)
+        _, fm, content = cls.__regex.split(string, 2)
+        metedata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
     def __init__(self, metedata, content):
-        data = metadata
-        self.data = ["content: content"]
+        self.data = metadata
+        self.data["content"] = content
 
-    def @property(self, body()):
+    @property
+    def body(self):
         return self.data["content"]
 
-    def @property(self, type()):
-        if self.data contains type:
-            return self.data["type"]
-        elif:
-            return None
+    @property
+    def type(self):
+        return self.data["type"] if "type" in self.data else None
 
-    def setter(self):
-        self.data["type"] = @property
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
 
     def __getitem__(self, key):
         return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        data = {}
+        for key, value in self.data.items[]:
+            if key != "content":
+                data[key] = value
+        return str(data)
